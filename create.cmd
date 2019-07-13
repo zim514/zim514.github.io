f96734727a13526f9225a70f642ca389c95e2528
zim514 <krisyoung@gmail.com>
2019-07-13 16:01:44 -0400

    Add ambi alpha to repo

--- create.cmd
+++ create.cmd
@@ -1,1 +1,1 @@
-py create_repository.py --datadir=repo https://github.com/zim514/zim514.github.io.git:repo/repository.snapcase  https://github.com/zim514/script.service.hue.git:script.service.hue
+py create_repository.py --datadir=repo https://github.com/zim514/zim514.github.io.git:repo/repository.snapcase  https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/script.service.hue.git#ambi:script.service.hue
