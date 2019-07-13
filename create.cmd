0c2077c65cd1a40cd8ae8d2c435f6a803650a8b6
zim514 <krisyoung@gmail.com>
2019-07-13 18:59:43 -0400

    .

--- create.cmd
+++ create.cmd
@@ -1,1 +1,2 @@
 py create_repository.py --datadir=repo https://github.com/zim514/zim514.github.io.git:repo/repository.snapcase  https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/script.service.hue.git#ambi:script.service.hue
+@pause
