264f199bf872a5c57e40c9c26168f4e7f4263e48
Snapcase <krisyoung@gmail.com>
2019-07-16 16:22:34 -0400

    automate git push in repo script

--- create.cmd
+++ create.cmd
@@ -1,2 +1,5 @@
 py create_repository.py --datadir=repo https://github.com/zim514/zim514.github.io.git:repo/repository.snapcase  https://github.com/zim514/script.service.hue.git:script.service.hue
+@git add *.xml *.zip *.md5
+@git commit -m "Repo update"
+@git push origin master
 @pause
