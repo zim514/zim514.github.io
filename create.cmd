9f194af921fa2506e343ff16ef39ae77e2b72719
Snapcase <krisyoung@gmail.com>
2019-12-25 10:30:46 -0500

    .

--- create.cmd
+++ create.cmd
@@ -2,5 +2,5 @@
 @py create_repository.py --datadir=repo https://github.com/zim514/zim514.github.io.git:repo/repository.snapcase  https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/script.module.pyrollbar.git:script.module.pyrollbar
 @git add *.xml *.zip *.md5
 @git commit -m "Automated repo update"
-rem @git push origin master
+@git push origin master
 @pause
