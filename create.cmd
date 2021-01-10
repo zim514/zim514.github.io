fc20a487f3b01928bd8383fff84a4502d7b0b077
Snapcase <krisyoung@gmail.com>
2021-01-10 14:59:25 -0500

    remove tests

--- create.cmd
+++ create.cmd
@@ -1,5 +1,5 @@
-@rem del /S /Q *.zip *.md5 *.xml
-@py create_repository.py --datadir=repo https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/script.module.pyrollbar.git:script.module.pyrollbar
+@del /S /Q *.zip *.md5 *.xml
+@py create_repository.py --datadir=repo https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/script.module.pyrollbar.git:script.module.pyrollbar https://github.com/zim514/zim514.github.io.git:repo/repository.snapcase
 @git add *.xml *.zip *.md5
 @git commit -m "Automated repo update"
 @git push origin master
@@ -7,7 +7,7 @@
 
 
 
-rem @py create_repository.py --datadir=repo https://github.com/zim514/zim514.github.io.git:repository.snapcase  https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/script.module.pyrollbar.git:script.module.pyrollbar
+
 
 
 
