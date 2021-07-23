650c57c1ef7a8be53fa3a279aa48885bcd8cdc6b
Snapcase <krisyoung@gmail.com>
2021-07-23 13:45:33 -0400

    Automated repo update

--- create.cmd
+++ create.cmd
@@ -1,4 +1,4 @@
-@del /S /Q *.zip *.md5
+rem @del /S /Q *.zip *.md5
 @py create_repository.py --datadir=repo https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/zim514.github.io.git:devrepo
 REM @py create_repository.py --datadir=repo https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/script.module.pyrollbar.git:script.module.pyrollbar https://github.com/zim514/zim514.github.io.git:repo/repository.snapcase
 @git add *.xml *.zip *.md5
