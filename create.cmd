c11f409eac4c1a5e79ca338686f5de33f885e227
Snapcase <krisyoung@gmail.com>
2021-07-23 13:27:07 -0400

    Move dev repo

--- create.cmd
+++ create.cmd
@@ -1,5 +1,5 @@
 @del /S /Q *.zip *.md5 *.xml
-@py create_repository.py --datadir=repo https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/zim514.github.io.git:repo/repository.snapcase
+@py create_repository.py --datadir=repo https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/zim514.github.io.git:devrepo
 REM @py create_repository.py --datadir=repo https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/script.module.pyrollbar.git:script.module.pyrollbar https://github.com/zim514/zim514.github.io.git:repo/repository.snapcase
 @git add *.xml *.zip *.md5
 @git commit -m "Automated repo update"
