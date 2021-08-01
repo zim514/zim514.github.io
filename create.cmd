8c571d72218827617601ef8d12751edad3166f11
Snapcase <krisyoung@gmail.com>
2021-08-01 10:22:49 -0400

    change branch name

--- create.cmd
+++ create.cmd
@@ -3,7 +3,7 @@
 @REM @py create_repository.py --datadir=repo https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/script.module.pyrollbar.git:script.module.pyrollbar https://github.com/zim514/zim514.github.io.git:repo/repository.snapcase
 @git add *.xml *.zip *.md5
 @git commit -m "Automated repo update"
-@git push origin master
+@git push origin main
 @pause
 
 
