cb2e84f96371da46acc702862c5e1301024bc862
Snapcase <krisyoung@gmail.com>
2021-08-07 19:38:54 -0400

    move pages

--- create.cmd
+++ create.cmd
@@ -1,5 +1,5 @@
 @del /S /Q *.zip *.md5
-@py create_repository.py --datadir=repo https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/zim514.github.io.git:repository.snapcase
+@py create_repository.py --datadir=docs/repo https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/zim514.github.io.git:repository.snapcase
 @REM @py create_repository.py --datadir=repo https://github.com/zim514/script.service.hue.git:script.service.hue https://github.com/zim514/script.module.pyrollbar.git:script.module.pyrollbar https://github.com/zim514/zim514.github.io.git:repo/repository.snapcase
 @git add *.xml *.zip *.md5
 @git commit -m "Automated repo update"
